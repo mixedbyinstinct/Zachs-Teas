1fce76a1a62525e23b5e4d870719dcf5e22a2c1c
Zach Baron <mixedbyinstinct@gmail.com>
2025-07-22 18:36:28 -0400

    Initial Commit

---
+++ Zach's Teas/Zach_s_TeasApp.swift
@@ -0,0 +1,17 @@
+//
+//  Zach_s_TeasApp.swift
+//  Zach's Teas
+//
+//  Created by Zach Baron on 7/22/25.
+//
+
+import SwiftUI
+
+@main
+struct Zach_s_TeasApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ Zach's TeasTests/Zach_s_TeasTests.swift
@@ -0,0 +1,17 @@
+//
+//  Zach_s_TeasTests.swift
+//  Zach's TeasTests
+//
+//  Created by Zach Baron on 7/22/25.
+//
+
+import Testing
+@testable import Zach_s_Teas
+
+struct Zach_s_TeasTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
